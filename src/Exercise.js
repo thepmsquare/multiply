9327cb65833956d456b6d66de1b43cac31392fd6
thePmSquare <impmmmm@gmail.com>
2020-08-19 12:27:22 +0530

    inital setup

---
+++ src/Exercise.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./stylesheets/Exercise.css";
+
+class Exercise extends Component {
+  render = () => {
+    return (
+      <div className="Exercise">
+        <h1>Exercise</h1>
+      </div>
+    );
+  };
+}
+
+export default Exercise;
