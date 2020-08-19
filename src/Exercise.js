cca93843b72bc47e7706f0d82e87cb9596b7fc35
thePmSquare <impmmmm@gmail.com>
2020-08-19 20:08:19 +0530

    add material ui

--- src/Exercise.js
+++ src/Exercise.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 import "./stylesheets/Exercise.css";
 
 class Exercise extends Component {
@@ -6,6 +7,9 @@ class Exercise extends Component {
     return (
       <div className="Exercise">
         <h1>Exercise</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   };
