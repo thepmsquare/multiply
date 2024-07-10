a96e4603d0db8cb076d3491432e4fd212d94a68b
thepmsquare <impmmmm@gmail.com>
2024-07-10 22:10:54 +0530

    💄 finalise layout for game

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,5 +1,21 @@
 "use client";
 
+import { IoMdHeart, IoMdTrophy } from "react-icons/io";
+import { MdSportsScore } from "react-icons/md";
+
+import ThemeToggleFAB from "@/components/ThemeToggleFAB";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Input,
+  Progress,
+} from "@nextui-org/react";
+import { semanticColors } from "@nextui-org/theme";
+
 export default function Game() {
   // state
 
@@ -9,5 +25,41 @@ export default function Game() {
 
   // misc
 
-  return <div>hi</div>;
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <Card className="max-w-[80%]">
+        <CardHeader className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <Button
+            startContent={<IoMdHeart className="text-danger text-xl" />}
+            className="col-span-2 md:col-span-1"
+          >
+            lives: 2
+          </Button>
+          <Button startContent={<MdSportsScore className="text-xl" />}>
+            score: 4
+          </Button>
+          <Button
+            startContent={<IoMdTrophy className="text-success text-xl" />}
+          >
+            high score: 7
+          </Button>
+        </CardHeader>
+        <Divider />
+        <CardBody className="flex flex-col gap-10 py-10 w-4/5 mx-auto">
+          <p className="text-center text-3xl">5 * 3 = </p>
+          <Input type="number" label="your answer" autoFocus />
+          <Progress aria-label="Loading..." value={60} className="max-w-md" />
+        </CardBody>
+        <Divider />
+        <CardFooter className="flex gap-4">
+          <Button className="w-full">skip</Button>
+          <Button color="danger" className="w-full">
+            exit
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <ThemeToggleFAB />
+    </div>
+  );
 }
