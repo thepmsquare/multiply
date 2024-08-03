a247820831c93d2dad8a2c944691e144182fee6b
thepmsquare <impmmmm@gmail.com>
2024-08-03 19:18:19 +0530

    🚧 start logic for game over

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -42,6 +42,7 @@ export default function Game() {
   const [score, changeScore] = useState(0);
   const [timeLeft, changeTimeLeft] = useState(10000);
   const [allowedTime, changeAllowedTime] = useState(10000);
+  const [isEnd, changeIsEnd] = useState(false);
   const timerId: MutableRefObject<null | NodeJS.Timeout> = useRef(null);
 
   // functions
@@ -110,6 +111,11 @@ export default function Game() {
       }, 1200);
     } else {
       // handle game end
+      changeIsTransitionVisible(true);
+      setTimeout(() => {
+        changeIsTransitionVisible(false);
+        changeIsEnd(true);
+      }, 1200);
     }
   };
 
@@ -121,6 +127,7 @@ export default function Game() {
       handleSkip();
     }
   }, [timeLeft]);
+
   useEffect(() => {
     changeCurrentRound(1);
     changeQuestion(getQuestion(currentRound));
@@ -135,86 +142,102 @@ export default function Game() {
 
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <Card className="max-w-[80%]">
-        <CardHeader className="grid grid-cols-2 md:grid-cols-3 gap-4">
-          <Button
-            startContent={<IoMdHeart className="text-danger text-xl" />}
-            className="col-span-2 md:col-span-1"
-          >
-            lives: <SlotCounter value={livesLeft} />
-          </Button>
-          <Button startContent={<MdSportsScore className="text-xl" />}>
-            score: <SlotCounter value={score} />
-          </Button>
-          <Button
-            startContent={<IoMdTrophy className="text-success text-xl" />}
-          >
-            high score: <SlotCounter value={7} />
-          </Button>
-        </CardHeader>
-        <Divider />
-        <CardBody className="flex flex-col gap-10 py-10 w-4/5 mx-auto text-center">
-          <p>Round: {currentRound}</p>
-          {question ? (
-            <div className="flex justify-center">
-              <SlotCounter
-                value={`${question.p1} * ${question.p2} = `}
-                charClassName="text-3xl m-1"
-              />
+      {!isEnd ? (
+        <>
+          <Card className="max-w-[80%]">
+            <CardHeader className="grid grid-cols-2 md:grid-cols-3 gap-4">
+              <Button
+                startContent={<IoMdHeart className="text-danger text-xl" />}
+                className="col-span-2 md:col-span-1"
+              >
+                lives: <SlotCounter value={livesLeft} />
+              </Button>
+              <Button startContent={<MdSportsScore className="text-xl" />}>
+                score: <SlotCounter value={score} />
+              </Button>
+              <Button
+                startContent={<IoMdTrophy className="text-success text-xl" />}
+              >
+                high score: <SlotCounter value={7} />
+              </Button>
+            </CardHeader>
+            <Divider />
+            <CardBody className="flex flex-col gap-10 py-10 w-4/5 mx-auto text-center">
+              <p>Round: {currentRound}</p>
+              {question ? (
+                <div className="flex justify-center">
+                  <SlotCounter
+                    value={`${question.p1} * ${question.p2} = `}
+                    charClassName="text-3xl m-1"
+                  />
 
-              <SlotCounter
-                value={isTransitionVisible ? question.correctAnswer : "?"}
-                charClassName={`text-3xl m-1 ${
-                  isTransitionVisible ? "text-success" : ""
-                }`}
+                  <SlotCounter
+                    value={isTransitionVisible ? question.correctAnswer : "?"}
+                    charClassName={`text-3xl m-1 ${
+                      isTransitionVisible ? "text-success" : ""
+                    }`}
+                  />
+                </div>
+              ) : (
+                <Spinner />
+              )}
+              <Input
+                type="number"
+                label="your answer"
+                autoFocus
+                value={userInput}
+                onChange={handleUserInputOnChange}
               />
-            </div>
-          ) : (
-            <Spinner />
-          )}
-          <Input
-            type="number"
-            label="your answer"
-            autoFocus
-            value={userInput}
-            onChange={handleUserInputOnChange}
-          />
-          <Progress
-            aria-label="Loading..."
-            value={(timeLeft / allowedTime) * 100}
-            className="max-w-md"
-          />
-        </CardBody>
-        <Divider />
-        <CardFooter className="flex gap-4">
-          <Button className="w-full" onPress={handleSkip}>
-            skip
-          </Button>
-          <Button color="danger" className="w-full" onPress={onOpen}>
-            exit
-          </Button>
-        </CardFooter>
-      </Card>
+              <Progress
+                aria-label="Loading..."
+                value={(timeLeft / allowedTime) * 100}
+                className="max-w-md"
+              />
+            </CardBody>
+            <Divider />
+            <CardFooter className="flex gap-4">
+              <Button className="w-full" onPress={handleSkip}>
+                skip
+              </Button>
+              <Button color="danger" className="w-full" onPress={onOpen}>
+                exit
+              </Button>
+            </CardFooter>
+          </Card>
+          <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+              {(onClose) => (
+                <>
+                  <ModalHeader className="flex flex-col gap-1">
+                    confirmation for exit
+                  </ModalHeader>
+                  <ModalFooter>
+                    <Button variant="light" onPress={onClose}>
+                      cancel
+                    </Button>
+                    <Link href="/">
+                      <Button color="danger">exit</Button>
+                    </Link>
+                  </ModalFooter>
+                </>
+              )}
+            </ModalContent>
+          </Modal>
+        </>
+      ) : (
+        <Card className="max-w-[80%]">
+          <CardHeader>the end</CardHeader>
+          <CardBody className="flex flex-col text-center">
+            your score: {score}
+          </CardBody>
+          <CardFooter>
+            <Link href="/">
+              <Button color="danger">exit</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      )}
 
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                confirmation for exit
-              </ModalHeader>
-              <ModalFooter>
-                <Button variant="light" onPress={onClose}>
-                  cancel
-                </Button>
-                <Link href="/">
-                  <Button color="danger">exit</Button>
-                </Link>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
       <ThemeToggleFAB />
     </div>
   );
